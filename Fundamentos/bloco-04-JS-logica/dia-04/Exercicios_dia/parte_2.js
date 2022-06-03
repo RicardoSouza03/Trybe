efc2e1b1266b0b0c2e0b61e770970620023b4771
Ricardo_Souza <rl03souza@gmail.com>
2022-06-03 15:37:35 -0300

    adicionando dia 04 bloco 04

--- Fundamentos/bloco-04-JS-logica/dia-04/Exercicios_dia/parte_2.js
+++ Fundamentos/bloco-04-JS-logica/dia-04/Exercicios_dia/parte_2.js
@@ -14,5 +14,39 @@ function verficandoPalindromo(palavra) {
 }
 verficandoPalindromo("ovo");
 
-function maiorValorDoArray() {}
-maiorValorDoArray();
+let arrayDeNumbers = [2, 3, 2, 5, 8, 2, 3];
+
+function valorDoArrayQueMaisSeRepete(numbers) {
+  let array = numbers;
+
+  array.sort();
+
+  let numeroTeste1 = array[0];
+  let numeroTeste2;
+  let vezes1 = 0;
+  let vezes2 = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === numeroTeste1) {
+      vezes1 += 1;
+    } else if (array[i] === numeroTeste2 || numeroTeste2 == undefined) {
+      vezes2 += 1;
+      numeroTeste2 = array[i];
+    } else {
+      if (vezes1 > vezes2) {
+        numeroTeste2 = array[i];
+        vezes2 = 0;
+      } else {
+        numeroTeste1 = array[i];
+        vezes1 = 0;
+      }
+    }
+  }
+
+  if (vezes1 > vezes2) {
+    return numeroTeste1;
+  } else {
+    return numeroTeste2;
+  }
+}
+console.log(valorDoArrayQueMaisSeRepete(arrayDeNumbers));
